0ef3862adeb1f6bc9d1c99ae0351b03dd28a4e1c
Guldar <ahunzanovaguldar@gmail.com>
2022-08-16 00:25:07 +0300

    Homework Six

---
+++ src/main/java/com/geekbrains/homewlrkLesson6/BasePageHome.java
@@ -0,0 +1,23 @@
+package com.geekbrains.homewlrkLesson6;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePageHome {
+    public WebDriver driver;
+    public WebDriverWait webDriverWait;
+    Actions actions;
+
+    public final static  String AFISHA_BASE_URL = "https://afisha.ru";
+
+    public  BasePageHome (WebDriver driver) {
+        this.driver = driver;
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        actions = new Actions(driver);
+        PageFactory.initElements(driver, this);
+    }
+}
---
+++ src/main/java/com/geekbrains/homewlrkLesson6/SuccessfulFramePopUp.java
@@ -0,0 +1,28 @@
+package com.geekbrains.homewlrkLesson6;
+
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class SuccessfulFramePopUp extends BasePageHome{
+    public SuccessfulFramePopUp(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//iframe[contains(@src, 'login')]")
+    private WebElement switchToFrame;
+
+    @FindBy(xpath = "//h2[.='Вход']")
+    private WebElement actualTitle;
+
+    public SuccessfulFramePopUp switchToFrameSet(String expectedTitle) {
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[contains(@src, 'login')]")));
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login")));
+        Assertions.assertEquals(expectedTitle, actualTitle.getText());
+        return this;
+    }
+
+}
